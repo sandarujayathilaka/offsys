7ffce2de3677d9e62b5639267c9b06c05bd3d94b
sandarujayathilaka <sandarujayathilaka26@gmail.com>
2023-07-21 08:30:09 +0530

    corrected responsive ag part

--- src/components/Ag/AgDepartment.jsx
+++ src/components/Ag/AgDepartment.jsx
@@ -42,10 +42,14 @@ function AgDepartment() {
 
   return (
     <div className="p-4 sm:ml-64 mt-[150px]">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
         {department &&
           department.map((dep, index) => (
-            <Link to={`/offsys/ag/allemployee/${dep.name}`} key={index}>
+            <Link
+              to={`/offsys/ag/allemployee/${dep.name}`}
+              key={index}
+              className="block"
+            >
               <div className="px-6 py-4 text-center bg-red-900 text-white transition-all duration-300 h-100 rounded-lg shadow-lg border-2 border-gray-300 hover:translate-y-[-5px] hover:bg-[#0A4754] hover:text-white">
                 {dep.name}
               </div>
